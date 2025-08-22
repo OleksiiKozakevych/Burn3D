9fc7f34479e5a9604cee2a2e426de2d6fee9bd06
OleksiiKozakevych <toller719@gmail.com>
2025-08-22 14:49:16 +0300

    Delete modules directory

--- modules/Grain.py
+++
@@ -1,9 +0,0 @@
-class grain:
-    def __init__(self, _path):
-        self.path = _path
-
-    def set_path(self, _path):
-        self.path = _path
-
-    def get_path(self):
-        return self.path
