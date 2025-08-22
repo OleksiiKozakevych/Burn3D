935852d91fa82213f6f81d63e6b933183afe789b
OleksiiKozakevych <toller719@gmail.com>
2025-08-22 14:49:01 +0300

    Delete node_master directory

--- node_master/launch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Activate the virtual environment
-call venv\Scripts\activate
-
-:: Start the three Python programs in separate command windows
-start cmd /k python .\main.py
--- node_master/node_editor/common.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from enum import Enum
-
-
-class Node_Status(Enum):
-    CLEAN = 1
-    DIRTY = 2
-    ERROR = 3
